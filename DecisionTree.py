4dbd3394d3b40b8bbb758c26aa93d426cb51432b
Yazdan ZandiyeVakili <yazdanzv.1378@gmail.com>
2023-04-03 04:16:56 +0430

    ss

---
+++ DecisionTree.py
@@ -0,0 +1,71 @@
+from Read_DataSet import *
+from sklearn.model_selection import cross_validate
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+
+
+def cross_validation(model, x_data, y_data, k):
+    scores = ['accuracy', 'precision', 'recall', 'f1']
+    answer = cross_validate(estimator=model, X=x_data, y=y_data,
+                            cv=k, scoring=scores, return_train_score=True)
+    results_train = {"Training Accuracy": answer['train_accuracy'],
+                     "Mean of Training Accuracy": answer['train_accuracy'].mean(),
+                     "Training Precision": answer['train_precision'],
+                     "Mean of Training Precision": answer['train_precision'].mean(),
+                     "Training Recall": answer['train_recall'],
+                     "Mean of Training Recall": answer['train_recall'].mean(),
+                     "Training F1_Score": answer['train_f1'], "Mean of Training F1_Score": answer['train_f1'].mean()}
+    results_test = {"Validation Accuracy": answer['test_accuracy'],
+                    "Mean of Validation Accuracy": answer['test_accuracy'].mean(),
+                    "Validation Precision": answer['test_precision'],
+                    "Mean of Validation Precision": answer['test_precision'].mean(),
+                    "Validation Recall": answer['test_recall'],
+                    "Mean of Validation Recall": answer['test_recall'].mean(),
+                    "Validation F1_Score": answer['test_f1'],
+                    "Mean of Validation F1_Score": answer['test_f1'].mean()}
+    return results_train, results_test
+
+
+def make_plot(x_label, y_label, plot_title, train_data, val_data):
+    plt.figure(figsize=(12, 6))
+    labels = ["1st Fold", "2nd Fold", "3rd Fold", "4th Fold", "5th Fold"]
+    X_axis = np.arange(len(labels))
+    ax = plt.gca()
+    plt.ylim(0.40000, 1)
+    plt.bar(X_axis - 0.2, train_data, 0.4, color='blue', label='Training')
+    plt.bar(X_axis + 0.2, val_data, 0.4, color='red', label='Validation')
+    plt.title(plot_title, fontsize=30)
+    plt.xticks(X_axis, labels)
+    plt.xlabel(x_label, fontsize=14)
+    plt.ylabel(y_label, fontsize=14)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+def make_decision_tree():
+    decision_tree = DecisionTreeClassifier(criterion="entropy", random_state=0, min_samples_split=5)
+    decision_tree_train_result, decision_tree_test_result = cross_validation(decision_tree, x_train, y_train, k=5)
+    print(decision_tree_train_result)
+    print()
+    print(decision_tree_test_result)
+    return decision_tree_train_result, decision_tree_test_result
+
+
+def show_plot():
+    number = 2
+    decision_tree_train_result, decision_tree_test_result = make_decision_tree()
+    model_name = "Decision Tree"
+    scores = ['Accuracy', 'Precision', 'Recall', 'F1_Score']
+    scores_descriptions = ["Accuracy in 5 Folds", "Precision in 5 Folds",
+                           "Recall in 5 Folds", "F1_Score in 5 Folds"]
+    train_keys = ["Training Accuracy", "Training Precision",
+                  "Training Recall", "Training F1_Score"]
+    test_keys = ["Validation Accuracy", "Validation Precision",
+                 "Validation Recall", "Validation F1_Score"]
+    make_plot(model_name, scores[number], scores_descriptions[number],
+              decision_tree_train_result[train_keys[number]],
+              decision_tree_test_result[test_keys[number]])
+
+
+show_plot()
